fbe902d6533ec8cd883ad2722b14d767b2f8c86b
alvarogarcia1010 <00111317@uca.edu.sv>
2021-03-08 21:03:30 -0600

    Issue #1

---
+++ pages/timeline/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const Timeline = () => {
+  return (
+    <div>
+      <h1>This is the timeline</h1>
+      <Link href="/"><a>Go home</a></Link>
+    </div>
+  )
+}
+
+export default Timeline
