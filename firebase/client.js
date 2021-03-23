5f9b037618f2b5d8b3159358ae876012d87f0963
alvarogarcia1010 <00111317@uca.edu.sv>
2021-03-23 07:38:36 -0600

    Issue #1

--- firebase/client.js
+++ firebase/client.js
@@ -11,7 +11,25 @@ const firebaseConfig = {
   appId: "1:31208006202:web:4907a21c0a5c290b658b03"
 };
 
-firebase.initializeApp(firebaseConfig)
+!firebase.apps.length && firebase.initializeApp(firebaseConfig)
+
+const mapUserFromFirebaseAuthToUser = user => {
+  const { displayName, email, photoURL } = user
+
+  return {
+    avatar: photoURL,
+    username: displayName,
+    email
+    
+  }
+}
+
+export const onAuthStateChanged = (onChange) => {
+  firebase.auth().onAuthStateChanged(user => {
+    const normalizedUser = mapUserFromFirebaseAuthToUser(user)
+    onChange(normalizedUser)
+  })
+}
 
 export const loginWithGithub = () => {
   const githubProvider = new firebase.auth.GithubAuthProvider()
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 import Head from 'next/head'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Button from '../components/Button/Button'
 import GitHub from '../components/Icons/Github'
-import { loginWithGithub } from '../firebase/client'
+import { loginWithGithub, onAuthStateChanged } from '../firebase/client'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [user, setUser] = useState(undefined)
+
+  useEffect(() => {
+    onAuthStateChanged(setUser)
+  }, [])
 
   const handleClick = () => {
-    //const [user, setUser] = useState(null)
     loginWithGithub()
       .then(user => {
-        const { additionalUserInfo } = user
-        const { username, profile} = additionalUserInfo
-        const { avatar_url, blog } = profile
-
-        console.log(user)
+        setUser(user)
       })
       .catch(error => console.log(error))
   }
@@ -31,10 +31,21 @@ export default function Home() {
         <img src='/logo.png' className={styles.logo} alt='logo' />
         <h1 className={styles.title}>Devter</h1>
         <h2 className={styles.subtitle}>Talk about development <br/> with developers</h2>
-        <Button onClick={handleClick}>
-          <GitHub width={24} height={24} fill="#ffffff" />
-          Login with GitHub
-        </Button>
+        
+        {user === null && (
+          <Button onClick={handleClick}>
+            <GitHub width={24} height={24} fill="#ffffff" />
+            Login with GitHub
+          </Button>
+        )}
+
+        {user && user.avatar && (
+          <div>
+            <img src={user.avatar} />
+            <strong>{user.username}</strong>
+          </div>
+        )}
+        
       </main>
     </div>
   )
