85fc3ad461cda2f018362c4f737e4a2821c4c793
alvarogarcia1010 <00111317@uca.edu.sv>
2021-05-10 12:09:10 -0600

    Issue #1

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -1,12 +1,22 @@
+import Head from "next/head"
 import Link from "next/link"
 
-const Timeline = () => {
+const Timeline = ({ username}) => {
   return (
     <div>
-      <h1>This is the timeline</h1>
+      <Head>
+        <title>Timeline - Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>This is the timeline of {username}</h1>
       <Link href="/"><a>Go home</a></Link>
     </div>
   )
+}
+
+Timeline.getInitialProps = () => {
+  return fetch('http://localhost:3000/api/hello')
+    .then(res => res.json())
 }
 
 export default Timeline
